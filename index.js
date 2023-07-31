1db60f9ccbba343ccaa7ea4d1b37af75dd189d75
lalit.tanwar <lalit.tanwar07@gmail.com>
2023-07-31 23:12:02 +0530

    package updates

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const config = require('config');
 
 module.exports = {
     get(key){
-        return process.env[key] || config.get(key)
+        let dotkey = key.replace(/\_/g,".").toLowerCase();
+        let _KEY = key.replace(/\./g,"_").toUpperCase();
+        if(process.env[dotkey] !==undefined ){
+            return process.env[dotkey];
+        } else if(process.env[_KEY] !==undefined ){
+            return process.env[_KEY];
+        }
+        return config.get(key);
     }
 }
