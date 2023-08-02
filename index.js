d9573e5f8aa86cb92b4b93dfcc515ed5291abab9
lalit.tanwar <lalit.tanwar07@gmail.com>
2023-08-02 14:47:30 +0530

    add getIfPresent

--- index.js
+++ index.js
@@ -11,5 +11,22 @@ module.exports = {
             return process.env[_KEY];
         }
         return config.get(key);
+    },
+    has(key){
+        let dotkey = key.replace(/\_/g,".").toLowerCase();
+        let _KEY = key.replace(/\./g,"_").toUpperCase();
+        if(process.env[dotkey] !==undefined ){
+            return true;
+        } else if(process.env[_KEY] !==undefined ){
+            return true;
+        }
+        return config.has(key);
+    },
+    getIfPresent(key,key2){
+        if(this.has(key)){
+            return this.get(key);
+        } else if(key2){
+            return this.get(key2);
+        }
     }
 }
